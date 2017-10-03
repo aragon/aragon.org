d01ffbdab662990e5ff1aef725f65b41a7692c0e
Pierre Bertet <hello@pierre.world>
2017-10-03 13:10:59 +0200

    section component

---
+++ toolkit/src/comps/section/section.vue
@@ -0,0 +1,31 @@
+<template>
+  <component
+    :is="visual? 'div' : 'section'"
+    :class="{'section--large': large}"
+    class="section"
+  >
+    <div class="section__in">
+      <slot />
+    </div>
+  </component>
+</template>
+
+<script>
+  export default {
+    props: {
+      large: Boolean,
+      visual: Boolean
+    }
+  }
+</script>
+
+<style>
+  @import '../../shared-styles.css';
+  .section__in {
+    margin: 0 auto;
+    max-width: var(--cols10);
+  }
+  .section--large .section__in {
+    max-width: var(--cols12);
+  }
+</style>
