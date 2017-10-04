550a5be22b7c1b94f5b444dd15077ed79c952a09
Pierre Bertet <hello@pierre.world>
2017-10-04 09:39:44 +0200

    unstoppable-organizations: background logo

--- components/home/unstoppable-organizations/unstoppable-organizations.vue
+++ components/home/unstoppable-organizations/unstoppable-organizations.vue
@@ -1,15 +1,17 @@
 <template>
-  <ag-section class="main">
-    <h1 class="title">Unstoppable organizations</h1>
-    <p class="subtitle">Create value without borders or intermediaries</p>
-    <div class="app-wrapper">
-      <img
-        class="app"
-        src="./assets/app-screen.svg"
-        width="1140"
-        height="508"
-        alt="Aragon App"
-      />
+  <ag-section class="unstoppable-orgs">
+    <div class="in">
+      <h1 class="title">Unstoppable organizations</h1>
+      <p class="subtitle">Create value without borders or intermediaries</p>
+      <div class="app-wrapper">
+        <img
+          class="app"
+          src="./assets/app-screen.svg"
+          width="1140"
+          height="508"
+          alt="Aragon App"
+        />
+      </div>
     </div>
   </ag-section>
 </template>
@@ -25,14 +27,11 @@
 
 <style scoped>
   @import '../../../toolkit/src/shared-styles.css';
-  .main {
+  .unstoppable-orgs {
     padding-top: 170px;
     text-align: center;
-    background-image: linear-gradient(
-      130deg,
-      var(--turquoise100),
-      var(--white)
-    );
+    background: url(./assets/background.svg) no-repeat 50% -60px,
+      linear-gradient(130deg, var(--turquoise100), var(--white)) no-repeat 0 0;
   }
   .title {
     @apply --text-xxlarge;
@@ -50,6 +49,7 @@
   .app {
     display: block;
     margin-left: 50%;
+    max-width: 100%;
     transform: translateX(-50%);
     flex-shrink: 0;
     text-align: center;
