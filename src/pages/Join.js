0be497feb321845f82b42ce663c5dcb34eb8153f
Luis Iván Cuende <me@luisivan.net>
2017-11-20 12:40:25 +0100

    Anchors for CTA in PageTitle

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -3,11 +3,13 @@ import { Button } from '@aragon/ui'
 import { PageTitle, Page } from '../components'
 import { Culture, Value } from '../components/Join'
 
+const Anchor = Button.withComponent('a')
+
 const Join = () => (
   <Page path="/join">
     <PageTitle title="Join us" subtitle="We are building the future of organizations">
-      <Button mode="outline">View open positions</Button>
-      <Button mode="strong">Apply now</Button>
+      <Anchor mode="outline" href="https://wiki.aragon.one/jobs">View open positions</Anchor>
+      <Anchor mode="strong" href="https://wiki.aragon.one/jobs">Apply now</Anchor>
     </PageTitle>
     <Culture />
     <Value />
