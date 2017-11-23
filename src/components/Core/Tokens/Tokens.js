a435dd28c00449a00a1fb1c03e958a70f0eef2f3
Pierre Bertet <hello@pierre.world>
2017-11-23 12:23:44 +0000

    Core/Tokens: responsive mode

--- src/components/Core/Tokens/Tokens.js
+++ src/components/Core/Tokens/Tokens.js
@@ -1,42 +1,64 @@
 import React from 'react'
-import { styled } from '@aragon/ui'
-import { Section, Text, theme } from '@aragon/ui'
+import {
+  styled,
+  styledCss as css,
+  Section,
+  Text,
+  font,
+  theme,
+  breakpoint,
+} from '@aragon/ui'
 
 import tokens from './assets/tokens.png'
 import background from './assets/background.svg'
 
+const medium = css => breakpoint('medium', css)
+const large = css => breakpoint('large', css)
+
 const Wrapper = styled.div`
   position: relative;
   z-index: 1;
-  margin-top: 200px;
+  pointer-events: none;
+  margin-top: calc(-200px + 5%);
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  background-position: 50% 0;
+  background-size: cover;
 `
 
 const Main = styled(Section)`
-  margin-top: calc(-200px - 235px);
-  padding-top: 235px;
-  background-image: url(${background});
-  background-repeat: no-repeat
-  background-position: 50% 0;
-  background-size: cover;
-
+  padding: 200px 15px 50px;
   text-align: center;
 
+  ${medium(css`
+    margin-top: calc(-240px + 5%);
+    padding-top: calc(200px + 5%);
+  `)};
+
   h1 {
-    font-size: 32px;
+    font-size: 25px;
+    font-weight: 400;
     margin-bottom: 50px;
   }
-
   img {
-    max-width: 750px;
+    max-width: calc(100% - 30px);
     margin-bottom: -5px;
   }
+  ${medium(`
+    font-size: 37px;
+    img {
+      max-width: 750px;
+    }
+  `)};
 `
 
 const Tokens = () => (
   <Wrapper>
     <Main>
       <div>
-        <Text heading="1" color={theme.gradientText}>Managing your organization has never been easier</Text>
+        <Text heading="1" color={theme.gradientText}>
+          Managing your organization has never been easier
+        </Text>
       </div>
       <img src={tokens} alt="" />
     </Main>
