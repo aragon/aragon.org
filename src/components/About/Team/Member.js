1dc0a614b52f3f0df016575b05326c0d107fc7ad
Pierre Bertet <hello@pierre.world>
2017-11-23 22:47:21 +0000

    About/Team: responsive

--- src/components/About/Team/Member.js
+++ src/components/About/Team/Member.js
@@ -9,18 +9,15 @@ import GitHubIcon from './assets/github.svg'
 const Main = styled(Section)`
   padding-top: 50px;
   text-align: center;
-
   .picture {
     width: 128px;
     height: 128px;
-    border-radius: 50%;
-    transition: all 0.5s;
+    transition: transform 0.5s ease-in-out;
+    will-change: transform;
   }
-
-  .picture:hover {
+  &:hover .picture {
     transform: translateY(-5px);
   }
-
   h1 {
     margin-top: 20px;
     font-size: 22px;
@@ -42,19 +39,54 @@ const Main = styled(Section)`
   }
 `
 
-const Member = ({ picture, name, title, twitter, medium, github }) => (
-  <Main>
+const Member = ({
+  className,
+  picture,
+  name,
+  title,
+  twitter,
+  medium,
+  github,
+}) => (
+  <Main className={className}>
     <div>
-      <img src={require(`./assets/pictures/${picture}`)} alt="" className="picture" />
+      <img
+        src={require(`./assets/pictures/${picture}`)}
+        alt=""
+        width="128"
+        height="128"
+        className="picture"
+      />
       <h1>{name}</h1>
       <h2>{title}</h2>
       <div className="socials">
-        {twitter ?
-          <a href={`https://twitter.com/${twitter}`} target="_blank" rel="noopener noreferrer"><img src={TwitterIcon} alt="" /></a> : null}
-        {medium ?
-          <a href={`https://medium.com/@${medium}`} target="_blank" rel="noopener noreferrer"><img src={MediumIcon} alt="" /></a> : null}
-        {github ?
-          <a href={`https://github.com/${github}`} target="_blank" rel="noopener noreferrer"><img src={GitHubIcon} alt="" /></a> : null}
+        {twitter ? (
+          <a
+            href={`https://twitter.com/${twitter}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={TwitterIcon} alt="" />
+          </a>
+        ) : null}
+        {medium ? (
+          <a
+            href={`https://medium.com/@${medium}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={MediumIcon} alt="" />
+          </a>
+        ) : null}
+        {github ? (
+          <a
+            href={`https://github.com/${github}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={GitHubIcon} alt="" />
+          </a>
+        ) : null}
       </div>
     </div>
   </Main>
--- src/components/About/Team/Team.js
+++ src/components/About/Team/Team.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { styled } from '@aragon/ui'
-import { Section, theme } from '@aragon/ui'
+import { styled, breakpoint, Section, theme } from '@aragon/ui'
 
 import SectionTitle from '../../SectionTitle'
 import Member from './Member'
 
-const columns = `
-  padding: 128px 0;
+const medium = css => breakpoint('medium', css)
+const large = css => breakpoint('large', css)
+
+const CoreSection = styled(Section)`
+  padding: 50px 0;
+  background: ${theme.mainBackground};
 
   .columns {
     display: flex;
@@ -14,15 +17,20 @@ const columns = `
     align-items: flex-start;
     margin: 50px 0;
   }
-  .columns > * {
-    width: 25%;
-  }
+
+  ${medium(`
+    padding: 120px 0;
+  `)};
 `
 
-const CoreSection = styled(Section)`
-  background: ${theme.mainBackground};
-
-  ${columns}
+const StyledMember = styled(Member)`
+  width: 50%;
+  ${medium(`
+    width: 33%;
+  `)};
+  ${large(`
+    width: 25%;
+  `)};
 `
 
 const Core = () => (
@@ -30,23 +38,87 @@ const Core = () => (
     <div className="core">
       <SectionTitle title="Core Team" />
       <div className="columns">
-        <Member picture="Luis.png" name="Luis Cuende" title="Project Lead" twitter="licuende" medium="lic" github="luisivan" />
-        <Member picture="Jorge.png" name="Jorge Izquierdo" title="Tech Lead" twitter="izqui9" medium="izqui9" github="izqui" />
-        <Member picture="Maria.png" name="Maria Gomez" title="Strategy Lead" twitter="MyPaoG" medium="MyPaoG" github="mariapao" />
-        <Member picture="Tatu.png" name="Tatu Karki" title="Communications Director" twitter="Smokyish" medium="Smokyish" github="Smokyish" />
-        <Member picture="Luke.png" name="Luke Duncan" title="Research Lead" twitter="lkngtn" medium="lkngtn" github="lkngtn" />
-        <Member picture="Oliver.png" name="Oliver Nordbjerg" title="Glue" twitter="ONordbjerg" github="onbjerg" />
-        <Member picture="Pierre.png" name="Pierre Bertet" title="UI & Interaction Developer" twitter="bpierre" github="bpierre" />
-        <Member picture="Brett.png" name="Brett Sun" title="EVM Engineer" twitter="sohkai" medium="sohkai" github="sohkai" />
+        <StyledMember
+          picture="Luis.png"
+          name="Luis Cuende"
+          title="Project Lead"
+          twitter="licuende"
+          medium="lic"
+          github="luisivan"
+        />
+        <StyledMember
+          picture="Jorge.png"
+          name="Jorge Izquierdo"
+          title="Tech Lead"
+          twitter="izqui9"
+          medium="izqui9"
+          github="izqui"
+        />
+        <StyledMember
+          picture="Maria.png"
+          name="Maria Gomez"
+          title="Strategy Lead"
+          twitter="MyPaoG"
+          medium="MyPaoG"
+          github="mariapao"
+        />
+        <StyledMember
+          picture="Tatu.png"
+          name="Tatu Karki"
+          title="Communications Director"
+          twitter="Smokyish"
+          medium="Smokyish"
+          github="Smokyish"
+        />
+        <StyledMember
+          picture="Luke.png"
+          name="Luke Duncan"
+          title="Research Lead"
+          twitter="lkngtn"
+          medium="lkngtn"
+          github="lkngtn"
+        />
+        <StyledMember
+          picture="Oliver.png"
+          name="Oliver Nordbjerg"
+          title="Glue"
+          twitter="ONordbjerg"
+          github="onbjerg"
+        />
+        <StyledMember
+          picture="Pierre.png"
+          name="Pierre Bertet"
+          title="UI & Interaction Developer"
+          twitter="bpierre"
+          github="bpierre"
+        />
+        <StyledMember
+          picture="Brett.png"
+          name="Brett Sun"
+          title="EVM Engineer"
+          twitter="sohkai"
+          medium="sohkai"
+          github="sohkai"
+        />
       </div>
     </div>
   </CoreSection>
 )
 
 const AdvisorsSection = styled(Section)`
+  padding: 50px 0;
   background: white;
 
-  ${columns}
+  .columns {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    margin: 50px 0;
+  }
+
+  ${medium(`
+    padding: 120px 0;
+  `)};
 `
 
 const Advisors = () => (
@@ -54,10 +126,30 @@ const Advisors = () => (
     <div className="advisors">
       <SectionTitle title="Advisors" />
       <div className="columns">
-        <Member picture="Mihai.jpg" name="Mihai Alisie" title="Ethereum cofounder and AKASHA founder" twitter="mihaialisie" />
-        <Member picture="Jake.jpg" name="Jake Brukhman" title="Cofounder at CoinFund" twitter="jbrukh" />
-        <Member picture="Kenny.jpg" name="Kenny Rowe" title="COO of Dai Foundation" twitter="kennyrowe" />
-        <Member picture="Brayton.jpg" name="Brayton Williams" title="Founding Partner at Boost VC" twitter="BraytonKey" />
+        <StyledMember
+          picture="Mihai.jpg"
+          name="Mihai Alisie"
+          title="Ethereum cofounder and AKASHA founder"
+          twitter="mihaialisie"
+        />
+        <StyledMember
+          picture="Jake.jpg"
+          name="Jake Brukhman"
+          title="Cofounder at CoinFund"
+          twitter="jbrukh"
+        />
+        <StyledMember
+          picture="Kenny.jpg"
+          name="Kenny Rowe"
+          title="COO of Dai Foundation"
+          twitter="kennyrowe"
+        />
+        <StyledMember
+          picture="Brayton.jpg"
+          name="Brayton Williams"
+          title="Founding Partner at Boost VC"
+          twitter="BraytonKey"
+        />
       </div>
     </div>
   </AdvisorsSection>
