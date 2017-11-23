da97aa8514f19ebd8e1ba0988889288c75940f7d
Pierre Bertet <hello@pierre.world>
2017-11-23 12:23:58 +0000

    Core/Customize: responsive mode

--- src/components/Core/Customize/Customize.js
+++ src/components/Core/Customize/Customize.js
@@ -1,73 +1,113 @@
 import React from 'react'
-import { styled } from '@aragon/ui'
-import { Section, Text, theme } from '@aragon/ui'
+import { styled, Section, Text, themeDark, breakpoint } from '@aragon/ui'
 
 import permission from './assets/permission.png'
 import settings from './assets/settings.svg'
 import path from './assets/path.svg'
 import code from './assets/code.svg'
 
-const Main = styled(Section)`
-  padding-top: 128px;
-  padding-bottom: 128px;
-  background-image: linear-gradient(39deg, #0b0b0a, #464f51);
-  text-align: center;
+const medium = css => breakpoint('medium', css)
+const large = css => breakpoint('large', css)
 
+const Main = styled(Section)`
+  background-image: linear-gradient(39deg, #0b0b0a, #464f51);
+`
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px 15px;
   h1 {
-    font-size: 32px;
+    font-size: 25px;
+    text-align: center;
     margin-bottom: 50px;
   }
-
-  img {
+  .screen {
     max-width: 750px;
-    margin-bottom: -5px;
+    width: 100%;
   }
-
   .columns {
-    display: flex;
-    flex-wrap: wrap;
-    align-items: flex-start;
-    margin: 50px 0;
+    padding-top: 10px;
+    max-width: 420px;
   }
-  .columns > div {
-    width: 33%;
-    padding: 0 25px;
-    text-align: left;
+  .item {
+    margin-top: 40px;
   }
-  .columns > div:nth-child(1) {
-    padding-left: 0;
-  }
-  .columns > div:nth-child(3) {
-    padding-right: 0;
-  }
-
-  p {
+  .item p {
     font-size: 16px;
-    color: ${theme.textSecondary};
+    color: ${themeDark.textSecondary};
     margin-top: 25px;
   }
+
+  ${medium(`
+    padding-top: 120px;
+    padding-bottom: 90px;
+    h1 {
+      font-size: 37px;
+    }
+    .item p {
+      font-size: 18px;
+    }
+    .columns {
+      display: flex;
+      flex-wrap: nowrap;
+      align-items: flex-start;
+      max-width: none;
+      padding-top: 70px;
+    }
+    .item {
+      width: 33%;
+      text-align: left;
+      margin-left: 30px;
+    }
+    .item:first-child {
+      margin-left: 0;
+    }
+  `)};
+  ${large(`
+    padding-bottom: 120px;
+    .columns {
+      padding-top: 60px;
+    }
+    .item {
+      margin-left: 80px;
+    }
+  `)};
 `
 
 const Customize = () => (
   <Main>
-    <div>
-      <Text heading="1" color={theme.gradientText}>It's your organization. Make it work for you</Text>
-    </div>
-    <img src={permission} alt="" />
-    <div className="columns">
+    <Content>
       <div>
-        <img src={settings} alt="" />
-        <p>Aragon organizations are powered by the AragonOS and fully customizable to fit your needs</p>
+        <Text heading="1" color={themeDark.textPrimary}>
+          It's your organization. Make it work for you.
+        </Text>
       </div>
-      <div>
-        <img src={path} alt="" />
-        <p>You set the rules of how and when actions are prompted and enacted </p>
+      <img className="screen" src={permission} alt="" />
+      <div className="columns">
+        <div className="item">
+          <img src={settings} alt="" />
+          <p>
+            Aragon organizations are powered by the AragonOS and fully
+            customizable to fit your needs.
+          </p>
+        </div>
+        <div className="item">
+          <img src={path} alt="" />
+          <p>
+            You set the rules of how and when actions are prompted and enacted.
+          </p>
+        </div>
+        <div className="item">
+          <img src={code} alt="" />
+          <p>
+            Code is open source. As a developer you can even modify it to match
+            your specific use case.
+          </p>
+        </div>
       </div>
-      <div>
-        <img src={code} alt="" />
-        <p>Code is open source. As a developer you can even modify it to match your specific use case</p>
-      </div>
-    </div>
+    </Content>
   </Main>
 )
 
