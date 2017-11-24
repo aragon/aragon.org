cdae84d7720e6fcff4ba4e30e54253d787fcb46c
Pierre Bertet <hello@pierre.world>
2017-11-24 12:14:29 +0000

    Remove unused SVG file

--- src/components/ReadyToTry/ReadyToTry.js
+++ src/components/ReadyToTry/ReadyToTry.js
@@ -10,7 +10,6 @@ import {
 } from '@aragon/ui'
 import AppScreenshot from '../AppScreenshot/AppScreenshot'
 
-import illustration from './assets/app-screen.svg'
 import lines from './assets/background-lines.svg'
 
 const medium = css => breakpoint('medium', css)
