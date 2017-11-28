7c0c28d33557ead36796ef79db3425be5ff6987f
Pierre Bertet <hello@pierre.world>
2017-11-28 17:54:24 +0000

    LogosBar: responsive

--- src/components/Home/LogosBar/LogosBar.js
+++ src/components/Home/LogosBar/LogosBar.js
@@ -10,9 +10,54 @@ import ibt from './assets/ibt.svg'
 import nasdaq from './assets/nasdaq.svg'
 import reuters from './assets/reuters.svg'
 
+const LOGOS = [
+  {
+    name: 'CoinDesk',
+    image: coindesk,
+    url: 'https://www.coindesk.com/dao-manager-aragon-alpha-ethereum/',
+    opacity: 0.5,
+  },
+  {
+    name: 'The Economist',
+    image: economist,
+    url:
+      'https://www.economist.com/news/world-if/21724906-trust-business-little-noticed-huge-startups-deploying-blockchain-technology-threaten',
+    opacity: 0.75,
+  },
+  {
+    name: 'Forbes',
+    image: forbes,
+    url:
+      'https://www.forbes.com/sites/rogeraitken/2017/04/20/forbes-under-30-tech-prodigy-in-cryptosale-for-decentralized-jurisdiction-platform/',
+    opacity: 0.75,
+  },
+  {
+    name: 'Reuters',
+    image: reuters,
+    url:
+      'https://www.reuters.com/article/us-aragon-blockchain-funding/blockchain-token-sale-nets-25-million-in-under-15-minutes-idUSKCN18E32X',
+    opacity: 0.75,
+  },
+  {
+    name: 'Nasdaq',
+    image: nasdaq,
+    url:
+      'http://www.nasdaq.com/article/disintermediating-entrepreneurship-iis-the-first-step-towards-a-fairer-world-cm806430',
+    opacity: 0.5,
+  },
+  {
+    name: 'IBT',
+    image: ibt,
+    url:
+      'http://www.ibtimes.co.uk/ethereum-based-aragon-leads-blockchain-exodus-slack-amid-phishing-scams-1640474',
+    opacity: 0.5,
+    width: 92,
+  },
+]
+
 const Main = styled(Section)`
   padding: 30px 0;
-  background: #FFF;
+  background: #fff;
   .title {
     text-align: center;
     text-transform: uppercase;
@@ -20,36 +65,30 @@ const Main = styled(Section)`
     margin-bottom: 10px;
   }
   .logos {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
     margin: 0 auto;
     width: 100%;
-    mix-blend-mode: multiply;
-    display: flex;
-    flex-direction: row;
     align-items: center;
-    justify-content: center;
-  }
-  a, img {
-    width: 128px;
-    max-height: 64px;
   }
   a {
-    flex: 1;
-    flex-basis: 0;
+    mix-blend-mode: multiply;
+    margin: 10px 20px;
+  }
+  a:first-child {
+    margin-left: 0;
+  }
+  a:first-child {
+    margin-right: 0;
+  }
+  img {
+    max-height: 64px;
     filter: grayscale(100%);
-    transition: all 200ms;
+    transition: filter 150ms ease-in-out;
   }
-  a:hover {
+  a:hover img {
     filter: none;
-  }
-  .coindesk, .ibt, .nasdaq {
-    filter: opacity(50%) grayscale(100%);
-  }
-  .forbes, .reuters, .economist {
-    filter: opacity(75%) grayscale(100%);
-  }
-  .ibt, .ibt img {
-    flex: 0;
-    width: 92px;
   }
 `
 
@@ -57,12 +96,17 @@ const LogosBar = () => (
   <Main>
     <h1 className="title">As seen on</h1>
     <div className="logos">
-      <a className="coindesk" href="https://www.coindesk.com/dao-manager-aragon-alpha-ethereum/" target="_blank" rel="noopener noreferrer"><img src={coindesk} alt="" /></a>
-      <a className="economist" href="https://www.economist.com/news/world-if/21724906-trust-business-little-noticed-huge-startups-deploying-blockchain-technology-threaten" target="_blank" rel="noopener noreferrer"><img src={economist} alt="" /></a>
-      <a className="forbes" href="https://www.forbes.com/sites/rogeraitken/2017/04/20/forbes-under-30-tech-prodigy-in-cryptosale-for-decentralized-jurisdiction-platform/" target="_blank" rel="noopener noreferrer"><img src={forbes} alt="" /></a>
-      <a className="reuters" href="https://www.reuters.com/article/us-aragon-blockchain-funding/blockchain-token-sale-nets-25-million-in-under-15-minutes-idUSKCN18E32X" target="_blank" rel="noopener noreferrer"><img src={reuters} alt="" /></a>
-      <a className="nasdaq" href="http://www.nasdaq.com/article/disintermediating-entrepreneurship-iis-the-first-step-towards-a-fairer-world-cm806430" target="_blank" rel="noopener noreferrer"><img src={nasdaq} alt="" /></a>    
-      <a className="ibt" href="http://www.ibtimes.co.uk/ethereum-based-aragon-leads-blockchain-exodus-slack-amid-phishing-scams-1640474" target="_blank" rel="noopener noreferrer"><img src={ibt} alt="" /></a>
+      {LOGOS.map(({ name, image, url, opacity, width = 128 }) => (
+        <a
+          key={url}
+          href={url}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ opacity }}
+        >
+          <img src={image} alt={name} title={name} width={width} />
+        </a>
+      ))}
     </div>
   </Main>
 )
