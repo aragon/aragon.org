58c893ed8b4b483a871ec4e5e5cf2431030277c7
Smokyish <smoky@kaerki.eu>
2018-08-08 20:01:14 +0300

    Updates

--- static.f1a2c087.js
+++ static.f1a2c087.js
@@ -2026,7 +2026,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var headerMenu = [['/core', 'Users'], ['https://hack.aragon.org', 'Developers'], ['/network', 'Network'], ['/foundation', 'Foundation'], ['/about', 'About'], ['/contribute', 'Contribute'], ['https://wiki.aragon.org', 'Wiki'], ['https://blog.aragon.one', 'Blog']];
+var headerMenu = [['/core', 'Users'], ['https://hack.aragon.org', 'Developers'], ['/network', 'Network'], ['/foundation', 'Foundation'], ['/about', 'About'], ['/contribute', 'Contribute'], ['https://wiki.aragon.org', 'Wiki'], ['https://blog.aragon.org', 'Blog']];
 
 var footerMenus = [[['Users', '/core'], ['Developers', 'https://hack.aragon.org/'], ['Network', '/network'], ['Contribute', '/contribute']], [['Wiki', 'https://wiki.aragon.org'], ['Blog', 'https://blog.aragon.one/'], ['Privacy Policy', 'https://wiki.aragon.org/documentation/legal/Privacy_policy/'], ['Terms of Use', 'https://wiki.aragon.org/documentation/legal/Terms_of_use_websites/']], [['*Try Aragon 0.5', 'https://app.aragon.one/'], ['Contact Us', 'mailto:contact@aragon.one'], ['Media/Press Inquiries', 'mailto:media@aragon.one'], ['Press Kit', 'https://wiki.aragon.one/press/press-kit/']], [['GitHub ', 'https://github.com/aragon', _github2.default], ['Twitter', 'https://twitter.com/AragonProject', _twitter2.default], ['Blog', 'https://blog.aragon.org/', _ghost2.default], ['Aragon Chat', 'https://aragon.chat/', _rocket2.default]]];
 
@@ -5370,4 +5370,4 @@ module.exports = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIiIGhlaWdodD0iMzIi
 /***/ })
 /******/ ]);
 });
-//# sourceMappingURL=static.b34454a7.js.map
+//# sourceMappingURL=static.f1a2c087.js.map
