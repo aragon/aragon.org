da2c1a9a9849ca31daf2d1a6db952971afcafa3f
Delfina <delfinapolito@gmail.com>
2018-10-30 14:33:45 +0100

    Not found page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import { PageTitle, Page } from '../components'
+import { Page } from '../components'
+import styled from 'styled-components'
+import Section from '../components/General/Section'
 
 const NotFound = () => (
   <Page>
-    <div>Not Found</div>
-    <div>The page you are trying to access could not be found.</div>
+  	<NotFoundSection>
+    <h2>Not Found</h2>
+    <h4>The page you are trying to access could not be found.</h4>
+    </NotFoundSection>
   </Page>
 )
 
+const NotFoundSection = styled(Section)`
+	height: 90vh;
+	display: flex;
+	align-items: center;
+	justyfy-content: center;
+`
 export default NotFound
