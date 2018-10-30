fcbcdb09a1b1f7e39dff5d8ad9b2fd53fc366971
Luis Cuende <me@luisivan.net>
2018-10-30 13:00:25 +0100

    CTAs for contribute

--- src/components/Project/ContributeContent.js
+++ src/components/Project/ContributeContent.js
@@ -6,16 +6,46 @@ import styled from 'styled-components'
 import { breakpoint } from '@aragon/ui'
 const medium = css => breakpoint('medium', css);
 
-const mudule1Content = (<p>If you are a developer, you can get started on any of the multiple Aragon repositories. For a smooth start, we have a list of items where we need help.</p>);
-const mudule2Content = (<p>We are looking for great people to expand the project. If you want to apply for a job, there are different positions open across multiple Aragon teams.</p>);
-const mudule3Content = (<p>Our vision is to decentralize Aragon’s development. There are different teams working on many parts of the project, but all aligned towards the same mission. The current teams are Aragon One and Aragon DAC.</p>);
+const mudule1Content = (
+  <span>
+    <p>If you are a developer, you can get started on any of the multiple Aragon repositories. For a smooth start, we have a list of items where we need help.</p>
+    <p>
+      <a className="action-button" href="https://github.com/issues?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+archived%3Afalse+label%3A%22good+first+issue%22+user%3Aaragon+" target="_blank">
+        <span>Good first issues <img src={require(`../General/assets/arrow.svg`)}/></span>
+      </a>
+    </p>  
+  </span>
+);
+const mudule2Content = (
+  <span>
+    <p>We are looking for great people to expand the project. If you want to apply for a job, there are different positions open across multiple Aragon teams.</p>
+    <p>
+      <a className="action-button" href="https://wiki.aragon.org/jobs" target="_blank">
+        <span>Apply <img src={require(`../General/assets/arrow.svg`)}/></span>
+      </a>
+    </p>  
+  </span>
+);
+const mudule3Content = (
+  <span>
+    <p>Our vision is to decentralize Aragon’s development. There are different teams working on many parts of the project, but all aligned towards the same mission. The current teams are Aragon One and Aragon DAC.</p>
+    <p>
+      <a className="action-button" href="https://aragon.one" target="_blank">
+        <span>Aragon One <img src={require(`../General/assets/arrow.svg`)}/></span>
+      </a>
+      <a className="action-button" href="https://blog.aragon.org/aragon-dac-a-new-community-effort-to-foster-aragons-development-led-by-giveth/" target="_blank">
+        <span>Aragon DAC <img src={require(`../General/assets/arrow.svg`)}/></span>
+      </a>
+    </p>  
+  </span>
+);
 
 const ContributeContent = () => (
   <Section>
     <Container>
-      <LongCard textAlign="left" background={'Project/assets/contribute-background1.png'} content={mudule3Content} image={'Project/assets/contribute3.svg'} colorWhite title="Multiple teams"></LongCard>
-      <Card imageUrl={'Project/assets/contribute1'} title="Help with open issues" content={mudule1Content}></Card>
-      <Card imageUrl={'Project/assets/contribute2'} title="Aragon teams are hiring" content={mudule2Content}></Card>
+      <LongCard linkTo="https://blog.aragon.org/flock-funding-for-aragon-teams/" textAlign="left" background={'Project/assets/contribute-background1.png'} content={mudule3Content} image={'Project/assets/contribute3.svg'} colorWhite title="Multiple teams"></LongCard>
+      <Card linkTo="https://github.com/issues?utf8=%E2%9C%93&q=is%3Aopen+is%3Aissue+archived%3Afalse+label%3A%22good+first+issue%22+user%3Aaragon+" imageUrl={'Project/assets/contribute1'} title="Help with open issues" content={mudule1Content}></Card>
+      <Card linkTo="https://wiki.aragon.org/jobs" imageUrl={'Project/assets/contribute2'} title="Aragon teams are hiring" content={mudule2Content}></Card>
     </Container>
   </Section>
 );
