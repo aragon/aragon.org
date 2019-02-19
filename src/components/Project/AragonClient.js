b255959ebf9fc6260d999217e6c0b6551840ffce
delfipolito <delfinapolito@gmail.com>
2019-02-19 10:40:07 -0300

    Team dropdown

--- src/components/Project/AragonClient.js
+++ src/components/Project/AragonClient.js
@@ -5,72 +5,132 @@ import styled from 'styled-components';
 import {breakpoint, Badge} from '@aragon/ui';
 const medium = css => breakpoint('medium', css);
 
-const AragonClient = () => (
-  <RoadmapSection>
-    <Container>
-      <RoadmapContainer>
-        <div className="roadmap-container">
-          <Entry entrypoint>
-            <Title>Current (Q1)</Title>
-            <Subtitle>
-              After a product is validated, it will be available for
-              visualization on the Dashboard and been added to publication
-              plans.
-            </Subtitle>
-          </Entry>
-          <Entry>
-            <Module>
-              <div className="badge-container">
-                <Badge>App center experience</Badge>
-              </div>
-              <h1>Responsive UI</h1>
-              <h3>
-                Make the platform and core apss responsive so Aragon can be used
-                from mobile browsers such as Status or Coinbase Wallet.
-              </h3>
-            </Module>
-          </Entry>
-          <Entry>
-            <Module>
-              <div className="badge-container">
-                <Badge>App center experience</Badge>
-              </div>
-              <h1>Individual identity: User profiles</h1>
-              <h3>
-                Allow individuals to create and manage their user profiles,
-                mapping their address to a human readable ENS name that can be
-                used to interact with apps within the organization.
-              </h3>
-            </Module>
-          </Entry>
-          <Entry>
-            <Module>
-              <div className="badge-container">
-                <AragonOneBadge>Aragon One</AragonOneBadge>
-                <Badge>App center experience</Badge>
-              </div>
-              <h1>Upgrading apps</h1>
-              <h3>
-                Provide users with a secure way to upgrade app versions, fixes
-                for feature or enhancements to already installed/in use
-                applications.
-              </h3>
-            </Module>
-          </Entry>
-          <Entry entrypoint>
-            <Title>Near-term (Q2 & Q3)</Title>
-            <Subtitle>
-              After a product is validated, it will be available for
-              visualization on the Dashboard and been added to publication
-              plans.
-            </Subtitle>
-          </Entry>
-        </div>
-      </RoadmapContainer>
-    </Container>
-  </RoadmapSection>
-);
+class AragonClient extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {active: 'all'};
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleChange(event) {
+    this.setState({active: event.target.value});
+  }
+
+  render() {
+    const {active} = this.state;
+    return (
+      <RoadmapSection>
+        <Container>
+          <DropdownContainer>
+            <h4>Select flock teams</h4>
+            <select value={this.state.active} onChange={this.handleChange}>
+              <option value="all">All</option>
+              <option value="aragon-one">Aragon One</option>
+              <option value="autark">Autark</option>
+            </select>
+          </DropdownContainer>
+          <RoadmapContainer>
+            <div className="roadmap-container">
+              <Entry entrypoint>
+                <Title>Current (Q1)</Title>
+                <Subtitle>
+                  After a product is validated, it will be available for
+                  visualization on the Dashboard and been added to publication
+                  plans.
+                </Subtitle>
+              </Entry>
+              {(this.state.active == 'all' ||
+                this.state.active == 'autark') && (
+                <Entry>
+                  <Module>
+                    <div className="badge-container">
+                      <Badge>App center experience</Badge>
+                    </div>
+                    <h1>Responsive UI</h1>
+                    <h3>
+                      Make the platform and core apss responsive so Aragon can
+                      be used from mobile browsers such as Status or Coinbase
+                      Wallet.
+                    </h3>
+                  </Module>
+                </Entry>
+              )}
+              {(this.state.active == 'all' ||
+                this.state.active == 'autark') && (
+                <Entry>
+                  <Module>
+                    <div className="badge-container">
+                      <Badge>App center experience</Badge>
+                    </div>
+                    <h1>Individual identity: User profiles</h1>
+                    <h3>
+                      Allow individuals to create and manage their user
+                      profiles, mapping their address to a human readable ENS
+                      name that can be used to interact with apps within the
+                      organization.
+                    </h3>
+                  </Module>
+                </Entry>
+              )}
+              {(this.state.active == 'all' ||
+                this.state.active == 'aragon-one') && (
+                <Entry>
+                  <Module>
+                    <div className="badge-container">
+                      <AragonOneBadge>Aragon One</AragonOneBadge>
+                      <Badge>App center experience</Badge>
+                    </div>
+                    <h1>Upgrading apps</h1>
+                    <h3>
+                      Provide users with a secure way to upgrade app versions,
+                      fixes for feature or enhancements to already installed/in
+                      use applications.
+                    </h3>
+                  </Module>
+                </Entry>
+              )}
+
+              <Entry entrypoint>
+                <Title>Near-term (Q2 & Q3)</Title>
+                <Subtitle>
+                  After a product is validated, it will be available for
+                  visualization on the Dashboard and been added to publication
+                  plans.
+                </Subtitle>
+              </Entry>
+            </div>
+          </RoadmapContainer>
+        </Container>
+      </RoadmapSection>
+    );
+  }
+}
+
+const DropdownContainer = styled.div`
+  display: flex;
+  max-width: 870px;
+  margin: auto;
+  padding: 0px 55px;
+  h4 {
+    line-height: 42px;
+    font-size: 18px;
+    color: #2d4051;
+    font-weight: 700;
+    padding-right: 30px;
+  }
+  select {
+    background: #ffffff;
+    border: 1px solid rgba(151, 151, 151, 0.16103);
+    box-sizing: border-box;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.0245131);
+    border-radius: 6px;
+    line-height: 42px;
+    font-size: 18px;
+    color: #2d4051;
+    width: calc(100% - 207px);
+    outline: none;
+  }
+`;
 const RoadmapContainer = styled.div`
   margin-top: 50px;
   .roadmap-container {
