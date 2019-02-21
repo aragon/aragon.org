f84c8b08a64414c95479474f8af58b450740b78d
delfipolito <delfinapolito@gmail.com>
2019-02-21 10:04:09 -0300

    Adding youtube to the footer

--- src/components/Page/Footer.js
+++ src/components/Page/Footer.js
@@ -1,17 +1,107 @@
-import React from 'react'
-import { Text, breakpoint, SafeLink } from '@aragon/ui'
-import { Link } from 'react-static'
-import styled from 'styled-components'
-import Section from '../General/Section'
-import logo from './assets/logo-footer.svg'
-import twitter from './assets/twitter.svg'
-import ghost from './assets/ghost.svg'
-import forum from './assets/forum.png'
-import rocket from './assets/rocket.svg'
+import React from 'react';
+import {Text, breakpoint, SafeLink} from '@aragon/ui';
+import {Link} from 'react-static';
+import styled from 'styled-components';
+import Section from '../General/Section';
+import logo from './assets/logo-footer.svg';
+import twitter from './assets/twitter.svg';
+import ghost from './assets/ghost.svg';
+import forum from './assets/forum.png';
+import rocket from './assets/rocket.svg';
+import youtube from './assets/youtube.svg';
 
 const medium = css => breakpoint('medium', css);
 const large = css => breakpoint('large', css);
 
+const Footer = ({...props}) => (
+  <FooterSection>
+    <Container>
+      <Item className="mobile-full">
+        <Link to="/">
+          <LogoImg src={logo} />
+        </Link>
+      </Item>
+      <Item>
+        <Link to="/">Home</Link>
+        <Link to="/discover">Discover</Link>
+        <Link to="/project">Project</Link>
+        <Link to="/network">Network</Link>
+      </Item>
+      <Item>
+        <a href="https://hack.aragon.org/" target="_blank">
+          Developers
+        </a>
+        <a href="https://wiki.aragon.org/dev/bug_bounty/" target="_blank">
+          Bug bounty
+        </a>
+        <Link to="/project/grants">Grants</Link>
+        <Link to="/project/governance">Governance</Link>
+      </Item>
+      <Item>
+        <Link to="/project/contribute">Contribute</Link>
+        <a href="https://blog.aragon.org/">Blog</a>
+        <a href="https://wiki.aragon.org/" target="_blank">
+          Wiki
+        </a>
+        <a href="https://aracon.one/" target="_blank">
+          AraCon
+        </a>
+      </Item>
+      <Item className="mobile-full">
+        <p>Be part of the conversation</p>
+        <Box>
+          <a href="https://twitter.com/aragonproject" target="_blank">
+            <img src={twitter} />
+          </a>
+          <a href="https://aragon.chat/" target="_blank">
+            <img src={rocket} />
+          </a>
+          <a href="https://forum.aragon.org/" target="_blank">
+            <Forum src={forum} />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCODiU_-FWhr4SVOoBlm-qaQ"
+            target="_blank">
+            <Youtube src={youtube} />
+          </a>
+        </Box>
+        <p>Subscribe to our newsletter</p>
+        <form
+          action="https://one.us15.list-manage.com/subscribe/post?u=a590aa3843a54b079d48e6e18&amp;id=e81a44c4bd"
+          method="post"
+          name="mc-embedded-subscribe-form"
+          target="_blank"
+          noValidate>
+          <div className="email-field">
+            <input
+              type="email"
+              name="EMAIL"
+              placeholder="Enter your email"
+              className="required email"
+              id="mce-EMAIL"
+            />
+            <div
+              aria-hidden="true"
+              style={{position: 'absolute', left: '-5000px'}}>
+              <input
+                type="text"
+                name="b_a590aa3843a54b079d48e6e18_e81a44c4bd"
+                tabIndex="-1"
+              />
+            </div>
+            <button
+              type="submit"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              className="button">
+              Subscribe
+            </button>
+          </div>
+        </form>
+      </Item>
+    </Container>
+  </FooterSection>
+);
 
 const FooterSection = styled(Section)`
   background-color: #f9fafc;
@@ -24,7 +114,7 @@ const Container = styled.div`
   grid-template-rows: auto;
   grid-column-gap: 15px;
   grid-row-gap: 15px;
-`
+`;
 
 const Item = styled.div`
   a {
@@ -39,7 +129,8 @@ const Item = styled.div`
   a:hover {
     opacity: 0.8;
   }
-  p, form {
+  p,
+  form {
     font-family: 'FontRegular', sans-serif;
     font-size: 14px;
     line-height: 3.14;
@@ -74,7 +165,7 @@ const Item = styled.div`
     ${medium('grid-column-end: inherit; grid-column-start: inherit;')};
     grid-column-end: 4;
   }
-`
+`;
 const Box = styled.div`
   margin: 0 -15px;
   a {
@@ -82,60 +173,17 @@ const Box = styled.div`
     padding: 0 15px;
     text-decoration: none;
   }
-`
+`;
 
-const Forum = styled.img `
+const Forum = styled.img`
   height: 19px;
   width: 19px;
-`
-const LogoImg = styled.img `
+`;
+const Youtube = styled.img`
+  margin-bottom: 1px;
+`;
+const LogoImg = styled.img`
   height: 50px;
-`
+`;
 
-const Footer = ({ ...props }) => (
-  <FooterSection>
-    <Container>
-      <Item className="mobile-full">
-          <Link to="/"><LogoImg src={logo} /></Link>
-      </Item>
-      <Item>
-        <Link to="/">Home</Link>
-        <Link to="/discover">Discover</Link>
-        <Link to="/project">Project</Link>
-        <Link to="/network">Network</Link>
-      </Item>
-      <Item>
-        <a href="https://hack.aragon.org/" target="_blank">Developers</a>
-        <a href="https://wiki.aragon.org/dev/bug_bounty/" target="_blank">Bug bounty</a>
-        <Link to="/project/grants">Grants</Link>
-        <Link to="/project/governance">Governance</Link>
-      </Item>
-      <Item>
-        <Link to="/project/contribute">Contribute</Link>
-        <a href="https://blog.aragon.org/">Blog</a>
-        <a href="https://wiki.aragon.org/" target="_blank">Wiki</a>
-        <a href="https://aracon.one/" target="_blank">AraCon</a>
-      </Item>
-      <Item className="mobile-full">
-        <p>Be part of the conversation</p>
-        <Box>
-          <a href="https://twitter.com/aragonproject" target="_blank"><img src={twitter} /></a>
-          <a href="https://aragon.chat/" target="_blank"><img src={rocket} /></a>
-          <a href="https://forum.aragon.org/" target="_blank"><Forum src={forum} /></a>
-        </Box>
-        <p>Subscribe to our newsletter</p>
-        <form action="https://one.us15.list-manage.com/subscribe/post?u=a590aa3843a54b079d48e6e18&amp;id=e81a44c4bd" method="post" name="mc-embedded-subscribe-form" target="_blank" noValidate>
-          <div className="email-field">
-            <input type="email" name="EMAIL" placeholder="Enter your email" className="required email" id="mce-EMAIL"/>
-            <div aria-hidden="true" style={{position:'absolute', left:'-5000px'}}>
-              <input type="text" name="b_a590aa3843a54b079d48e6e18_e81a44c4bd" tabIndex="-1" />
-            </div>
-            <button type="submit" name="subscribe" id="mc-embedded-subscribe" className="button">Subscribe</button>
-          </div>
-        </form>
-      </Item>
-    </Container>
-  </FooterSection>
-);
-
-export default Footer
+export default Footer;
